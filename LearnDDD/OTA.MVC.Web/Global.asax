813eaaa0d585eff42599da421e7b62cb9fe719b7
jialungithub <454112207@qq.com>
2017-04-10 16:13:10 +0800

    项目初建
    MVC

---
+++ LearnDDD/OTA.MVC.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OTA.MVC.Web.MvcApplication" Language="C#" %>
---
+++ LearnDDD/OTA.MVC.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OTA.MVC.Web.Startup))]
+namespace OTA.MVC.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
